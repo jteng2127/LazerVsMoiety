578acae5c8c57eb890d98dbacaef262118281d18
jteng2127 <jteng2127@gmail.com>
2021-11-09 14:54:32 +0800

    cleaning up

--- Assets/Scripts/DefendGrid.cs
+++ Assets/Scripts/DefendGrid.cs
@@ -9,32 +9,12 @@ public class DefendGrid : MonoBehaviour
     public int tileRowTotal = 5;
     public int tileColumnTotal = 9;
 
-    private List<List<GameObject> > defendTile = new List<List<GameObject> >();
+    // private List<List<GameObject> > defendTile = new List<List<GameObject> >();
     private float tileWidth = 0;
     private float tileHeight = 0;
 
     void Awake()
     {
-        // BoxCollider2D box = defendTilePrefab.GetComponent<BoxCollider2D>();
-        // tileWidth = box.size.x;
-        // tileHeight = box.size.y;
-        BoxCollider2D totalBox = GetComponent<BoxCollider2D>();
-        tileWidth = totalBox.size.x / tileColumnTotal;
-        tileHeight = totalBox.size.y / tileRowTotal;
-        for(int i = 0; i < tileRowTotal; i++){
-            List<GameObject> tileCol = new List<GameObject>();
-            for(int j = 0; j < tileColumnTotal; j++){
-                Vector3 deltaPosition = new Vector3(tileWidth*(j-tileColumnTotal/2), tileHeight*(i-tileRowTotal/2), 0);
-                tileCol.Add(Instantiate(
-                    defendTilePrefab,
-                    transform.position + deltaPosition,
-                    Quaternion.identity,
-                    gameObject.transform
-                    ) as GameObject);
-                tileCol[j].GetComponent<BoxCollider2D>().size = new Vector2(tileWidth, tileHeight);
-            }
-            defendTile.Add(tileCol);
-        }
     }
 
     void Start()
@@ -44,5 +24,23 @@ public class DefendGrid : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public GameObject create_tile(int row, int col){
+        BoxCollider2D totalBox = GetComponent<BoxCollider2D>();
+        tileWidth = totalBox.size.x / tileColumnTotal;
+        tileHeight = totalBox.size.y / tileRowTotal;
+
+        float dx = tileWidth*(col-tileColumnTotal/2);
+        float dy = tileHeight*(row-tileRowTotal/2);
+        Vector3 deltaPosition = new Vector3(dx, dy, 0);
+        GameObject tile = Instantiate(
+            defendTilePrefab,
+            transform.position + deltaPosition,
+            Quaternion.identity,
+            gameObject.transform
+        ) as GameObject;
+        tile.GetComponent<BoxCollider2D>().size = new Vector2(tileWidth, tileHeight);
+        return tile;
     }
 }
--- Assets/Scripts/manager/GameManager.cs
+++ Assets/Scripts/manager/GameManager.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private List<List<DefendTile> > defendTile = new List<List<DefendTile> >();
+    public GameObject defendGrid;
+    private List<List<DefendTile> > defendTileList = new List<List<DefendTile> >();
+    public static int tileRowTotal = 5;
+    public static int tileColumnTotal = 9;
+    private GameObject[,] defendTileArray = new GameObject[tileRowTotal, tileColumnTotal];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(10);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void create_tiles(){
+        for(int i = 0; i < tileRowTotal; i++){
+            List<DefendTile> tileCol = new List<DefendTile>();
+            for(int j = 0; j < tileColumnTotal; j++){
+                defendTileArray[i, j] = defendGrid.GetComponent<DefendGrid>().create_tile(i, j);
+            }
+            defendTileList.Add(tileCol);
+        }
     }
 }

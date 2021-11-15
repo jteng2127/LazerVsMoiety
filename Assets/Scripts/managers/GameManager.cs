10905eb5f4201e4077c24c30ebeeac1eada9f9f2
jteng2127 <jteng2127@gmail.com>
2021-11-15 10:55:43 +0800

    grid init in GameManager

--- Assets/Scripts/managers/GameManager.cs
+++ Assets/Scripts/managers/GameManager.cs
@@ -6,14 +6,14 @@ public class GameManager : MonoBehaviour
 {
     public GameObject defendGrid;
     private List<List<DefendTile> > defendTileList = new List<List<DefendTile> >();
-    public static int tileRowTotal = 5;
-    public static int tileColumnTotal = 9;
+    public const int tileRowTotal = 5;
+    public const int tileColumnTotal = 9;
     private GameObject[,] defendTileArray = new GameObject[tileRowTotal, tileColumnTotal];
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(10);
+        create_tiles();
 
     }
 
@@ -25,11 +25,11 @@ public class GameManager : MonoBehaviour
 
     void create_tiles(){
         for(int i = 0; i < tileRowTotal; i++){
-            List<DefendTile> tileCol = new List<DefendTile>();
+            // List<DefendTile> tileCol = new List<DefendTile>();
             for(int j = 0; j < tileColumnTotal; j++){
                 defendTileArray[i, j] = defendGrid.GetComponent<DefendGrid>().create_tile(i, j);
             }
-            defendTileList.Add(tileCol);
+            // defendTileList.Add(tileCol);
         }
     }
 }

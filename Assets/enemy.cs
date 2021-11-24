8b7be528bc10ad922ebf2fc3cecb48bad0d2c012
jteng2127 <jteng2127@gmail.com>
2021-11-24 09:56:40 +0800

    add enemy script

---
+++ Assets/enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f,  -0.1f));
+    }
+}

a8091030e9270899bdf0a5752bcb9c9fd2ca8eaf
jteng2127 <jteng2127@gmail.com>
2021-11-24 14:12:28 +0800

    create unit and enemy prefab, complete collide detection

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed;
+    void FixedUpdate()
+    {
+        // GetComponent<Rigidbody2D>().velocity = new Vector2(-3.0f, 0.0f);
+        transform.position = transform.position + new Vector3(-speed, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/unit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy"){
+            Debug.Log(collision.gameObject);
+            Destroy(collision.gameObject);
+            Destroy(transform.gameObject);
+        }
+    }
+}

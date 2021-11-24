ff0e202904a9083ebcefcb6d4eefc2feafaa905d
jimmyhealer <yahing6066@gmail.com>
2021-11-24 11:50:36 +0800

    www 不會寫QQ

--- Assets/Scripts/roles/RoleManager.cs
+++ Assets/Scripts/roles/RoleManager.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class RoleType
+{
+    string _type;
+    string roleType {get {return _type;}}
+
+    public RoleType(int type){
+        _type = type;
+    }
+}
+
 public class RoleManager: MonoBehaviour
 {
+    public RoleType roleType;
     public const string pictureSrc; // picture Src
     public double transparency; // picture transparency
     
@@ -32,4 +43,37 @@ public class RoleManager: MonoBehaviour
         _move(des, speed);
     }
 
+    void _transparent(double alpha = 1.0f)
+    {
+        // change transparency
+        transparency = alpha;
+        var render = gameObject.GetComponentInChildren(Renderer);    
+        render.material.color.a = alpha;
+    }
+
+    public void show()
+    {
+        _transparent(1.0f)
+    }
+
+    public void hide()
+    {
+        _transparent(0.0f)
+    }
+
+    public void destory()
+    {
+        Destory();
+    }
+}
+
+public class FunctionalGroup : RoleManager
+{
+    public string functinoalgroup;
+    public int blood;
+}
+
+public class Wave : RoleManager
+{
+    public int wave;
 }

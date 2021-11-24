cf3699fe502bda0cdb306dae97103acfc8ab0c3d
jteng2127 <jteng2127@gmail.com>
2021-11-24 14:26:15 +0800

    little optimize

--- Assets/Scripts/unit.cs
+++ Assets/Scripts/unit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class unit : MonoBehaviour
 {
-    void OnTriggerStay2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Enemy"){
             Debug.Log(collision.gameObject);

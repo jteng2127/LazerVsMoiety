992192a8a8a4434746fa6ce4c9db51114c19a9b3
jteng2127 <jteng2127@gmail.com>
2021-12-28 04:49:21 +0800

    add Editor and PlayWithStartScene script

---
+++ Assets/Editor/PlayWithStartScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.IO;
+
+public class PlayWithStartScene
+{
+    [MenuItem("Play/Execute starting scene _%h")]
+    public static void RunMainScene()
+    {
+        Scene currentScene = EditorSceneManager.GetActiveScene();
+        
+        File.WriteAllText(".lastScene",currentScene.name);
+        EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+        EditorApplication.isPlaying = true;
+    }
+   
+    [MenuItem("Play/Reload editing scnee _%g")]
+    public static void ReturnToLastScene()
+    {
+        string lastScene=File.ReadAllText(".lastScene");
+        EditorSceneManager.OpenScene("Assets/Scenes/" + lastScene + ".unity");
+    }
+}

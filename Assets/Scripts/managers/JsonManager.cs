dd7cd4d36f6328bb597439e273f4cf0f3b6d635a
jimmyhealer <yahing6066@gmail.com>
2021-12-29 01:27:21 +0800

    change JsonManger format

--- Assets/Scripts/managers/JsonManager.cs
+++ Assets/Scripts/managers/JsonManager.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class JsonManager : MonoBehaviour {
 
-  #region QueryEnemyAndAlly
+    #region QueryEnemyAndAlly
 
-  [SerializeField]
-  private class EnemyAndAllyData {
-    // functaional Group and wave
-    public int enemy_unit_id;
-    public int ally_unit_id;
-  }
+    [SerializeField]
+    private class EnemyAndAllyData {
+        // functaional Group and wave
+        public int enemy_unit_id;
+        public int ally_unit_id;
+    }
 
-  /// <summary>
-  /// Query the Enemy and Ally data list from the Json.
-  /// </summary>
-  /// <returns> 
-  /// Return the Enemy and Ally datalist. <br/>
-  /// Data list format: <br/>
-  /// [ {"enemy_unit_id": 1, "ally_unit_id": 1100},...]
-  /// </returns>
-  public List<EnemyAndAllyData> QueryEnemyAndAllyDataList() {
-    string load_enemy_and_ally_data = File.ReadAllText("../jsons/EnemyAndAlly.json");
-    List<EnemyAndAllyData> data_list = JsonUtility.FromJson<List<EnemyAndAllyData>>(load_enemy_and_ally_data);
-    return data_list;
-  }
+    /// <summary>
+    /// Query the Enemy and Ally data list from the Json.
+    /// </summary>
+    /// <returns> 
+    /// Return the Enemy and Ally datalist. <br/>
+    /// Data list format: <br/>
+    /// [ {"enemy_unit_id": 1, "ally_unit_id": 1100},...]
+    /// </returns>
+    public List<EnemyAndAllyData> QueryEnemyAndAllyDataList() {
+        string load_enemy_and_ally_data = File.ReadAllText("../jsons/EnemyAndAlly.json");
+        List<EnemyAndAllyData> data_list = JsonUtility.FromJson<List<EnemyAndAllyData>>(load_enemy_and_ally_data);
+        return data_list;
+    }
 
-  #endregion
+    #endregion
 
-  #region QueryEnemy
+    #region QueryEnemy
 
-  [SerializeField]
-  private class EnemyUnitData {
-    public int enemy_unit_id;
-    public string name;
-  }
+    [SerializeField]
+    private class EnemyUnitData {
+        public int enemy_unit_id;
+        public string name;
+    }
 
-  /// <summary>
-  /// Query the enemy unit data from the Json.
-  /// </summary>
-  public string QueryEnemyUnitData(int enemy_unit_query_id) {
-    string load_enemy_unit_data = File.ReadAllText("../jsons/EnemyUnit.json");
-    List<EnemyUnitData> data_list = JsonUtility.FromJson<List<EnemyUnitData>>(load_enemy_unit_data);
-    return data_list[enemy_unit_query_id].name;
-  }
+    /// <summary>
+    /// Query the enemy unit data from the Json.
+    /// </summary>
+    public string QueryEnemyUnitData(int enemy_unit_query_id) {
+        string load_enemy_unit_data = File.ReadAllText("../jsons/EnemyUnit.json");
+        List<EnemyUnitData> data_list = JsonUtility.FromJson<List<EnemyUnitData>>(load_enemy_unit_data);
+        return data_list[enemy_unit_query_id].name;
+    }
 
-  #endregion
+    #endregion
 }

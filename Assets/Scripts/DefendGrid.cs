362b9c262b9fed825e71e0a0a64fae4bbbab28b9
jteng2127 <jteng2127@gmail.com>
2021-12-28 14:49:55 +0800

    add new GameManager
    rename old Managers folder to old_managers
    rename old GameManager.cs to old_...
    
    GameManager use singleton
    add SceneType enum

--- Assets/Scripts/DefendGrid.cs
+++ Assets/Scripts/DefendGrid.cs
@@ -17,8 +17,8 @@ public class DefendGrid : MonoBehaviour
     void Awake()
     {
         BoxCollider2D totalBox = GetComponent<BoxCollider2D>();
-        tileWidth = totalBox.size.x / GameManager.tileColumnTotal;
-        tileHeight = totalBox.size.y / GameManager.tileRowTotal;
+        tileWidth = totalBox.size.x / old_GameManager.tileColumnTotal;
+        tileHeight = totalBox.size.y / old_GameManager.tileRowTotal;
     }
 
     void Start()
@@ -30,8 +30,8 @@ public class DefendGrid : MonoBehaviour
     }
 
     public GameObject create_tile(int row, int col){
-        float dx = tileWidth*(col-GameManager.tileColumnTotal/2);
-        float dy = tileHeight*(row-GameManager.tileRowTotal/2);
+        float dx = tileWidth*(col-old_GameManager.tileColumnTotal/2);
+        float dy = tileHeight*(row-old_GameManager.tileRowTotal/2);
         Vector3 deltaPosition = new Vector3(dx, dy, 0);
         GameObject tile = Instantiate(
             defendTilePrefab,
@@ -44,7 +44,7 @@ public class DefendGrid : MonoBehaviour
     }
 
     public GameObject spawn_enemy(int row){
-        float dy = tileHeight*(row-GameManager.tileRowTotal/2);
+        float dy = tileHeight*(row-old_GameManager.tileRowTotal/2);
         Vector3 deltaPosition = new Vector3(enemySpawnX, dy, 0);
         GameObject enemy = Instantiate(
             enemyPrefab,
--- Assets/Scripts/managers/GameManager.cs
+++ Assets/Scripts/managers/GameManager.cs
@@ -1,54 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
-  public const int tileRowTotal = 5;
-  public const int tileColumnTotal = 9;
+public class GameManager : ScriptableObject
+{
+    #region Singleton
+    protected static GameManager s_Instance;
 
-  private GameObject[,] defendTileArray = new GameObject[tileRowTotal, tileColumnTotal];
-
-  public GameObject defendGridObject;
-  private DefendGrid defendGrid;
-  public float enemySpawnInterval;
-  private float enemySpawnDelay;
-
-  public GameObject cardConveyorObject;
-  private CardConveyor cardConveyor;
-  public float cardSpawnInterval;
-  private float cardSpawnDelay;
-
-  void Start() {
-    defendGrid = defendGridObject.GetComponent<DefendGrid>();
-    cardConveyor = cardConveyorObject.GetComponent<CardConveyor>();
-    if (enemySpawnInterval == 0.0f) enemySpawnInterval = 3.0f;
-    if (cardSpawnInterval == 0.0f) cardSpawnInterval = 3.0f;
-    create_tiles();
-  }
-
-  void Update() {
-    // enemy spawn
-    if (enemySpawnDelay <= 0.0f) enemySpawnDelay = enemySpawnInterval;
-    enemySpawnDelay -= Time.deltaTime;
-    if (enemySpawnDelay <= 0.0f) {
-      int spawnRow = Random.Range(0, 5);
-      defendGrid.spawn_enemy(spawnRow);
-      Debug.Log("Enemy spawn at " + spawnRow);
+    public static GameManager Instance
+    {
+        get
+        {
+            if(s_Instance == null)
+            {
+                Debug.Log("GameManager: Create new instance.");
+                s_Instance = ScriptableObject.CreateInstance<GameManager>();
+            }
+            return s_Instance;
+        }
     }
 
-    // card spawn
-    if (cardSpawnDelay <= 0.0f) cardSpawnDelay = cardSpawnInterval;
-    cardSpawnDelay -= Time.deltaTime;
-    if (cardSpawnDelay <= 0.0f) {
-      cardConveyor.spawn_card();
-    }
-  }
+    #endregion
 
-  void create_tiles() {
-    for (int i = 0; i < tileRowTotal; i++) {
-      for (int j = 0; j < tileColumnTotal; j++) {
-        defendTileArray[i, j] = defendGrid.create_tile(i, j);
-      }
+    void OnEnable()
+    {
+        Debug.Log("GameManager Enable");
     }
-  }
+
+    #region Scene
+    public enum SceneType
+    {
+        Loading,
+        Login,
+        Menu,
+        User,
+        Setting,
+        StageSelect,
+        Stage,
+        GameOver,
+    }
+
+
+    public void Test()
+    {
+        Debug.Log("test game manager");
+    }
 }

4f05520edd2daa464926c6ef835c8eb8a751c33c
jteng2127 <jteng2127@gmail.com>
2021-12-28 20:05:59 +0800

    sign in button, adjust resolution setting

--- Assets/Scripts/Button/SignInSceneButton.cs
+++ Assets/Scripts/Button/SignInSceneButton.cs
@@ -4,16 +4,58 @@ using UnityEngine;
 
 public class SignInSceneButton : MonoBehaviour
 {
-    public void test(){
-        Debug.Log("test");
+    RectTransform _rectTransform;
+    GameObject _signInWindow;
+
+    Vector3 _startPosition;
+    Vector3 _signInWindowPosition;
+    bool _isOpenSignInWindow;
+    bool _isSignedIn;
+
+    void Awake(){
+        // Initial variables
+        _signInWindow = GameObject.Find("SignInWindow");
+        _rectTransform = transform.GetComponent<RectTransform>();
+        _startPosition = new Vector3(0.0f, -335.5f, 0.0f);
+        _signInWindowPosition = new Vector3(0.0f, -265.0f, 0.0f);
+
+        // Initial state
+        _isSignedIn = false;
+        _isOpenSignInWindow = false;
+        _signInWindow.SetActive(false);
+        _rectTransform.anchoredPosition3D = _startPosition;
     }
-    void Start()
-    {
+
+    #region Click behavior
+
+    void TriggerSignInWindow(){
+        if(_isOpenSignInWindow == true){
+            _isOpenSignInWindow = false;
+            _signInWindow.SetActive(false);
+            _rectTransform.anchoredPosition3D = _startPosition;
+            _isSignedIn = true;
+        }
+        else{
+            _isOpenSignInWindow = true;
+            _signInWindow.SetActive(true);
+            _rectTransform.anchoredPosition3D = _signInWindowPosition;
+        }
+    }
+
+    void CheckSignedIn(){
+        if(_isSignedIn){
+            
+        }
+    }
+
+    #endregion
+
+    #region interface
+
+    public void Click(){
+        TriggerSignInWindow();
         
     }
 
-    void Update()
-    {
-        
-    }
+    #endregion
 }

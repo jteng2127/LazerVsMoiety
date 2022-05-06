fe64db0ee42055535fb0d20f4888b602f906c9fa
jimmyhealer <yahing6066@gmail.com>
2022-05-06 16:54:42 +0800

    Initial FireStore

---
+++ Assets/Scripts/firebase/FireStoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase;
+
+public class FireStoreManager : MonoBehaviour {
+    #region Singleton
+
+    protected static FireStoreManager s_Instance;
+
+    public static FireStoreManager Instance {
+        [RuntimeInitializeOnLoadMethod]
+        get {
+            if (s_Instance == null) {
+                Debug.Log("FireStoreManager: Create new instance.");
+                CreateDefault();
+            }
+            return s_Instance;
+        }
+    }
+
+    static void CreateDefault() {
+        GameObject go = new GameObject("FireStoreManager", typeof(FireStoreManager));
+        DontDestroyOnLoad(go);
+        s_Instance = go.GetComponent<FireStoreManager>();
+    }
+
+    void OnEnable() {
+        Debug.Log("FireStoreManager Enable");
+        _init();
+    }
+
+    #endregion
+
+    private FirebaseFirestore db;
+}

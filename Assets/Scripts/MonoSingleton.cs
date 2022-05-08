2716aed957525bda5103f3a6fa036e2eec3cbc14
jteng2127 <jteng2127@gmail.com>
2022-05-09 00:06:17 +0800

    add MonoSingleton base class

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Mono singleton generic class, can't initialize on load method
+// -> can't use Update() when game start without calling Instance
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T _instance;
+    private static object _instanceLock = new object();
+
+    public static T Instance {
+        get {
+            lock(_instanceLock) {
+                if (_instance == null) {
+                    _instance = GameObject.FindObjectOfType<T>();
+                    if (_instance == null) {
+                        GameObject singleton = new GameObject(typeof(T).ToString());
+                        _instance = singleton.AddComponent<T>();
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public static int testStatic = 0;
+}
